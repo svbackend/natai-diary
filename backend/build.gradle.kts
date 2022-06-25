d5336938daddf9ec3ea55345ba68aae882908835
Valentyn Saik <svbackend22@gmail.com>
2022-06-25 13:32:19 +0200

    Update ktor to 2.0.2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,8 +2,8 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
-val jdbiVersion = "3.28.0"
-val flywayVersion = "8.5.10"
+val jdbiVersion = "3.30.0"
+val flywayVersion = "8.5.12"
 val exposedVersion = "0.38.2"
 
 plugins {
@@ -27,7 +27,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.postgresql:postgresql:42.3.4")
+    implementation("org.postgresql:postgresql:42.3.6")
     implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
@@ -41,7 +41,7 @@ dependencies {
     implementation("io.ktor:ktor-server-cors:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
-    implementation("org.kodein.di:kodein-di:7.11.0")
+    implementation("org.kodein.di:kodein-di:7.12.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.jdbi:jdbi3-core:$jdbiVersion")
     implementation("org.jdbi:jdbi3-json:$jdbiVersion")
