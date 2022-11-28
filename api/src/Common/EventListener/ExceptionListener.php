1389bf0ac42b2155e5f8169f9e28ffe38235b9ca
Valentyn Saik <svbackend22@gmail.com>
2022-11-27 21:15:59 -0500

    Revert changes to handle more exceptions than needed

--- api/src/Common/EventListener/ExceptionListener.php
+++ api/src/Common/EventListener/ExceptionListener.php
@@ -6,8 +6,6 @@ use App\Common\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Validator\ConstraintViolation;
 
 
@@ -18,16 +16,10 @@ class ExceptionListener
         // You get the exception object from the received event
         $exception = $event->getThrowable();
 
-        match (true) {
-            $exception instanceof ValidationException => $this->handleValidationException($event, $exception),
-            $exception instanceof AuthenticationException => $this->handleAuthenticationException($event, $exception),
-            $exception instanceof AccessDeniedException => $this->handleAccessDeniedException($event, $exception),
-            default => null,
-        };
-    }
+        if ($exception instanceof ValidationException === false) {
+            return;
+        }
 
-    private function handleValidationException(ExceptionEvent $event, ValidationException $exception): void
-    {
         $errors = $exception->getViolations();
 
         $response = new JsonResponse([
@@ -44,26 +36,6 @@ class ExceptionListener
                 ];
             }, iterator_to_array($errors)),
         ], Response::HTTP_BAD_REQUEST);
-
-        $event->setResponse($response);
-    }
-
-    private function handleAuthenticationException(ExceptionEvent $event, AuthenticationException $exception): void
-    {
-        $response = new JsonResponse([
-            'code' => 'authentication_error',
-            'message' => $exception->getMessage(),
-        ], Response::HTTP_UNAUTHORIZED);
-
-        $event->setResponse($response);
-    }
-
-    private function handleAccessDeniedException(ExceptionEvent $event, AccessDeniedException $exception): void
-    {
-        $response = new JsonResponse([
-            'code' => 'access_denied',
-            'message' => $exception->getMessage(),
-        ], Response::HTTP_FORBIDDEN);
 
         $event->setResponse($response);
     }
