45d173647855cbfab7707b1c84c09b05b8074e05
Valentyn Saik <svbackend22@gmail.com>
2023-01-11 17:19:58 -0500

    Move from schema:upd to migrations

---
+++ api/migrations/Version20230111221748.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230111221748 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE api_token_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE api_token (id INT NOT NULL, user_id UUID NOT NULL, api_token VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_7BA2F5EB7BA2F5EB ON api_token (api_token)');
+        $this->addSql('CREATE INDEX IDX_7BA2F5EBA76ED395 ON api_token (user_id)');
+        $this->addSql('COMMENT ON COLUMN api_token.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN api_token.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE change_email_token (token VARCHAR(255) NOT NULL, user_id UUID NOT NULL, new_email VARCHAR(255) NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(token))');
+        $this->addSql('CREATE INDEX IDX_DA195BEEA76ED395 ON change_email_token (user_id)');
+        $this->addSql('COMMENT ON COLUMN change_email_token.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN change_email_token.expires_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE confirmation_token (token VARCHAR(255) NOT NULL, user_id UUID NOT NULL, type VARCHAR(255) NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(token))');
+        $this->addSql('CREATE INDEX IDX_C05FB297A76ED395 ON confirmation_token (user_id)');
+        $this->addSql('COMMENT ON COLUMN confirmation_token.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN confirmation_token.expires_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE note (id UUID NOT NULL, user_id UUID NOT NULL, title VARCHAR(255) DEFAULT NULL, content TEXT DEFAULT NULL, actual_date DATE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_CFBDFA14A76ED395 ON note (user_id)');
+        $this->addSql('COMMENT ON COLUMN note.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN note.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN note.actual_date IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN note.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN note.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN note.deleted_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE note_tag (id UUID NOT NULL, note_id UUID NOT NULL, tag VARCHAR(255) NOT NULL, score INT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_737A976326ED0855 ON note_tag (note_id)');
+        $this->addSql('COMMENT ON COLUMN note_tag.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN note_tag.note_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE "users" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, is_email_verified BOOLEAN DEFAULT false NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON "users" (email)');
+        $this->addSql('COMMENT ON COLUMN "users".id IS \'(DC2Type:uuid)\'');
+        $this->addSql('ALTER TABLE api_token ADD CONSTRAINT FK_7BA2F5EBA76ED395 FOREIGN KEY (user_id) REFERENCES "users" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE change_email_token ADD CONSTRAINT FK_DA195BEEA76ED395 FOREIGN KEY (user_id) REFERENCES "users" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE confirmation_token ADD CONSTRAINT FK_C05FB297A76ED395 FOREIGN KEY (user_id) REFERENCES "users" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA14A76ED395 FOREIGN KEY (user_id) REFERENCES "users" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE note_tag ADD CONSTRAINT FK_737A976326ED0855 FOREIGN KEY (note_id) REFERENCES note (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE api_token_id_seq CASCADE');
+        $this->addSql('ALTER TABLE api_token DROP CONSTRAINT FK_7BA2F5EBA76ED395');
+        $this->addSql('ALTER TABLE change_email_token DROP CONSTRAINT FK_DA195BEEA76ED395');
+        $this->addSql('ALTER TABLE confirmation_token DROP CONSTRAINT FK_C05FB297A76ED395');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT FK_CFBDFA14A76ED395');
+        $this->addSql('ALTER TABLE note_tag DROP CONSTRAINT FK_737A976326ED0855');
+        $this->addSql('DROP TABLE api_token');
+        $this->addSql('DROP TABLE change_email_token');
+        $this->addSql('DROP TABLE confirmation_token');
+        $this->addSql('DROP TABLE note');
+        $this->addSql('DROP TABLE note_tag');
+        $this->addSql('DROP TABLE "users"');
+    }
+}
