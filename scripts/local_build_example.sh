40780fa09a0b57f53db85a2b2952a4e194eeefe0
Valentyn Saik <svbackend22@gmail.com>
2023-03-03 22:48:26 -0500

    Remove api-queue image since it's the same as api-php-fpm

--- scripts/local_build_example.sh
+++ scripts/local_build_example.sh
@@ -7,11 +7,8 @@ rf -rf "/tmp/natai/*"
 docker compose build api-php-fpm api-queue frontend
 
 docker save natai/api-php-fpm > /tmp/natai/api-php-fpm.tar
-docker save natai/api-queue > /tmp/natai/api-queue.tar
 docker save natai/frontend > /tmp/natai/frontend.tar
 
 rsync -avzh /tmp/natai/api-php-fpm.tar user@server:/home/deploy/natai/docker/
-
-rsync -avzh /tmp/natai/api-queue.tar user@server:/home/deploy/natai/docker/
 
 rsync -avzh /tmp/natai/frontend.tar user@server:/home/deploy/natai/docker/
