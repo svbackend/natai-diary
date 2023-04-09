a7bbdcef0e1a75aa4226f2631a5a04c32e6b6681
Valentyn Saik <svbackend22@gmail.com>
2023-04-08 21:19:42 -0400

    Add delete profile api endpoint

--- api/src/Auth/Controller/DeleteProfileAction.php
+++ api/src/Auth/Controller/DeleteProfileAction.php
@@ -2,23 +2,14 @@
 
 namespace App\Auth\Controller;
 
-use App\Auth\DTO\UserDto;
-use App\Auth\Entity\ApiToken;
 use App\Auth\Entity\User;
-use App\Auth\Http\Request\VerifyEmailRequest;
-use App\Auth\Http\Response\LoginSuccessResponse;
-use App\Auth\OpenApi\Ref\LoginErrorRef;
-use App\Auth\OpenApi\Ref\LoginRequestRef;
-use App\Auth\OpenApi\Ref\VerifyEmailResponseRef;
-use App\Auth\Repository\ApiTokenRepository;
-use App\Auth\Repository\ConfirmationTokenRepository;
 use App\Common\Controller\BaseAction;
 use App\Common\Http\Response\HttpOutputInterface;
 use App\Common\OpenApi\Ref\ServerErrorRef;
-use App\Common\OpenApi\Ref\ValidationErrorResponseRef;
 use Doctrine\ORM\EntityManagerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -27,41 +18,42 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
  * @OA\Tag(name="Auth")
  * @see VerifyEmailActionTest
  */
-class VerifyEmailAction extends BaseAction
+class DeleteProfileAction extends BaseAction
 {
     public function __construct(
-        private ConfirmationTokenRepository $tokens,
         private EntityManagerInterface $em,
+        private LoggerInterface $logger
     )
     {
     }
 
     /**
-     * @OA\RequestBody(@Model(type=VerifyEmailRequest::class))
      * @OA\Response(response=204, description="success")
-     * @OA\Response(response=400, description="validation error", @Model(type=ValidationErrorResponseRef::class))
-     * @OA\Response(response=404, description="not found", @Model(type=VerifyEmailResponseRef::class))
-     * @OA\Response(response=422, description="token expired", @Model(type=VerifyEmailResponseRef::class))
      * @OA\Response(response=500, description="server error", @Model(type=ServerErrorRef::class))
      */
-    #[Route('/api/v1/verify-email', methods: ['POST'])]
+    #[Route('/api/v1/me', methods: ['DELETE'])]
     public function __invoke(
-        VerifyEmailRequest $request,
+        #[CurrentUser] User $user
     ): HttpOutputInterface
     {
-        $token = $this->tokens->findOneBy(['token' => $request->token]);
+        $conn = $this->em->getConnection();
 
-        if (!$token) {
-            return $this->error('token_not_found', Response::HTTP_NOT_FOUND);
+        $conn->beginTransaction();
+
+        $randomEmail = uniqid('deleted-') . '@' . uniqid('deleted-') . '.com';
+
+        try {
+            $conn->delete('api_token', ['user_id' => $user->getId()]);
+            $conn->update('users', ['email' => $randomEmail, 'name' => $randomEmail], ['id' => $user->getId()]);
+            $conn->update('note', ['title' => '[deleted]', 'content' => '[deleted]'], ['user_id' => $user->getId()]);
+
+            $conn->commit();
+        } catch (\Exception $e) {
+            $conn->rollBack();
+
+            $this->logger->error($e->getMessage(), $e->getTrace());
+            return $this->error('server_error', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        if (!$token->isValid()) {
-            return $this->error('token_expired');
-        }
-
-        $token->getUser()->verifyEmail();
-        $this->tokens->remove($token);
-        $this->em->flush();
 
         return $this->success();
     }
