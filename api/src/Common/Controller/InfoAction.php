4ffb698210b841eb0f0922001dade015cc6426e6
Valentyn Saik <svbackend22@gmail.com>
2023-04-09 12:10:32 -0400

    Hide server info

--- api/src/Common/Controller/InfoAction.php
+++ api/src/Common/Controller/InfoAction.php
@@ -12,13 +12,6 @@ class InfoAction extends BaseAction
     #[Route('/api/v1/info', methods: ['GET'])]
     public function __invoke(Request $request): Response
     {
-        $headers = ['x-forwarded-for', 'x-forwarded-host', 'x-forwarded-proto', 'x-real-ip'];
-
-        $headersValues = array_map(
-            fn($header) => [$header => $request->headers->get($header)],
-            $headers
-        );
-
         return $this->json(
             [
                 'ip' => ClientIp::fromRequest($request),
