63dd9ce90097a90386e97c20c2f159c7fab979ec
Valentyn Saik <svbackend22@gmail.com>
2023-04-15 08:30:23 -0400

    Fix issue with canonical urls

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App({Component, pageProps}: AppProps) {
 
     return (
         <NextIntlProvider messages={messages} locale={locale}>
-            <GlobalSeo canonical={router.asPath}/>
+            <GlobalSeo canonical={router.pathname}/>
             <Script data-domain={"natai.app"} src="https://plausible.ukraidian.com/js/plausible.js"/>
 
             <Script
