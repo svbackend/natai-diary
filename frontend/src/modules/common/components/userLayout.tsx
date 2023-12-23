905bd61d094d090ea23ba8e48ee3f040a1754bf7
Valentyn Saik <svbackend22@gmail.com>
2023-12-23 11:11:27 +0100

    Fix issue when full page reload results in empty user variable

--- frontend/src/modules/common/components/userLayout.tsx
+++ frontend/src/modules/common/components/userLayout.tsx
@@ -5,18 +5,20 @@ import {useAppStateManager} from "../state";
 import {NotLoggedIn} from "./NotLoggedIn";
 import MainLayout from "./mainLayout";
 
+export const UserLoadingScreen = (
+    <div className="h-screen w-screen flex justify-center items-center dark:bg-gray-800">
+        <div className="flex flex-col items-center">
+            <Image src={flowerSvg} alt="Flower" width={100} height={100} className={"animate-spin"}/>
+            <p className="text-gray-600 dark:text-gray-300">Loading...</p>
+        </div>
+    </div>
+)
+
 function UserLayout({children, containerClass}: { children: ReactNode, containerClass?: string }) {
     const {user, isLoading, isLoaded} = useAppStateManager()
 
     if (!isLoaded) {
-        return (
-            <div className="h-screen w-screen flex justify-center items-center dark:bg-copyright">
-                <div className="flex flex-col items-center">
-                    <Image src={flowerSvg} alt="Flower" width={100} height={100} className={"animate-spin"}/>
-                    <p className="text-gray-600 dark:text-gray-300">Loading...</p>
-                </div>
-            </div>
-        )
+        return UserLoadingScreen
     }
 
     if (!user) {
